8a446046a982280b1cd55fe126cb2d081711c839
fzucchelli <fzucchelli@gmail.com>
2019-01-26 22:36:21 +0600

    Updates

---
+++ precache-manifest.f0aa3042db01231a4d855ef84e9a9f3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/addressBooknew/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "618d7955a9264cc1da81",
+    "url": "/addressBooknew/static/css/main.78943a3a.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/addressBooknew/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "1b15e90aa7f50f205062",
+    "url": "/addressBooknew/static/js/1.1b15e90a.chunk.js"
+  },
+  {
+    "revision": "251c0ae0ca5ea3598847",
+    "url": "/addressBooknew/static/js/runtime~main.251c0ae0.js"
+  },
+  {
+    "revision": "618d7955a9264cc1da81",
+    "url": "/addressBooknew/static/js/main.618d7955.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/addressBooknew/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/addressBooknew/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/addressBooknew/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "1b15e90aa7f50f205062",
+    "url": "/addressBooknew/static/css/1.0b5e1ed3.chunk.css"
+  },
+  {
+    "revision": "4ac71876674f2b63fff04652d6249807",
+    "url": "/addressBooknew/index.html"
+  }
+];
